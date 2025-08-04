cc839b0fe41792a461562230963e4cf61756f936
unfaded <unfaded@startmail.com>
2025-08-04 03:34:20 -0500

    Added OpenAI to providers

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,5 +1,7 @@
 pub mod ollama;
 pub mod anthropic;
+pub mod openai;
 
 pub use ollama::{OllamaClient, Model, ListModelsResponse, OllamaOptions};
 pub use anthropic::{AnthropicClient};
+pub use openai::{OpenAIClient};
