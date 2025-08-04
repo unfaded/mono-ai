8d20dabf56fa9436ea5962749a581ad0407f5500
unfaded <unfaded@startmail.com>
2025-08-04 08:31:46 -0500

    Added OpenRouter to providers

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,7 +1,9 @@
 pub mod ollama;
 pub mod anthropic;
 pub mod openai;
+pub mod openrouter;
 
 pub use ollama::{OllamaClient, Model, ListModelsResponse, OllamaOptions};
 pub use anthropic::{AnthropicClient};
 pub use openai::{OpenAIClient};
+pub use openrouter::{OpenRouterClient};
